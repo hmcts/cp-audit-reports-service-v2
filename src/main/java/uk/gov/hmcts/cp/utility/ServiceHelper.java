9646c121b8e1a3580224b7ecd183b06ca754e2cc
Nick Talbot <nick.talbot@hmcts.net>
2026-01-29 23:53:52 +0000

    PMD analysis updates

--- src/main/java/uk/gov/hmcts/cp/utility/ServiceHelper.java
+++ src/main/java/uk/gov/hmcts/cp/utility/ServiceHelper.java
@@ -8,9 +8,9 @@ import java.util.function.Function;
 public interface ServiceHelper {
 
     static <T, R, E> ResponseEntity<E> responseOk(
-            List<T> list,
-            Function<T, R> toResult,
-            Function<List<R>, E> toWrapper
+            final List<T> list,
+            final Function<T, R> toResult,
+            final Function<List<R>, E> toWrapper
     ) {
         return ResponseEntity.ok(toWrapper.apply(list.stream().map(toResult).toList()));
     }
