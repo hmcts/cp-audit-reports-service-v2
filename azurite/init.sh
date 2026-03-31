ece104dde4f6064fe8a9b55ac842ac7e318d4b03
Nick Talbot <nick.talbot@hmcts.net>
2026-03-31 10:08:15 +0100

    Config update

--- azurite/init.sh
+++ azurite/init.sh
@@ -3,4 +3,5 @@
 az storage table create --name reportrequests
 
 # az group create --location uksouth --name downloads-group
+
 # az container create --resource-group downloads-group --name downloads-container
